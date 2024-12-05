8d4e9ae106ec3baa70c1dbc9a682976bb207871b
k-gorbenko <164647543+k-gorbenko@users.noreply.github.com>
2024-12-05 22:04:06 +0300

    Update template_p01.py

--- Deep Learning/template_p01.py
+++ Deep Learning/template_p01.py
@@ -15,16 +15,16 @@ def softmax(vector):
 
 def multiplicative_attention(decoder_hidden_state, encoder_hidden_states, W_mult):
     attention_scores = W_mult @ decoder_hidden_state  # (n_features_enc, 1)
-    attention_weights = np.dot(encoder_hidden_states.T, attention_scores)  # (n_states, 1)
-    attention_weights = np.exp(attention_weights) / np.sum(np.exp(attention_weights), axis=0)
-    attention_vector = np.dot(encoder_hidden_states, attention_weights)  # (n_features_enc, 1)
-    return attention_vector  # Возвращаем вектор в виде (n_features_enc, 1)
+    attention_weights = np.dot(attention_scores.T, encoder_hidden_states)  # (1, n_states)
+    attention_weights = softmax(attention_weights)  # (1, n_states)
+    attention_vector = attention_weights.dot(encoder_hidden_states.T).T  # (n_features_enc, 1)
+    return attention_vector
 
 def additive_attention(decoder_hidden_state, encoder_hidden_states, v_add, W_add_enc, W_add_dec):
     encoder_transformed = W_add_enc @ encoder_hidden_states  # (n_features_int, n_states)
     decoder_transformed = W_add_dec @ decoder_hidden_state  # (n_features_int, 1)
-    scores = v_add @ (encoder_transformed + decoder_transformed.T)  # (1, n_states)
+    scores = v_add.T @ (encoder_transformed + decoder_transformed.T)  # (1, n_states)
     scores = np.tanh(scores)  # (1, n_states)
-    attention_weights = np.exp(scores) / np.sum(np.exp(scores), axis=1, keepdims=True)  # (1, n_states)
+    attention_weights = softmax(scores)  # (1, n_states)
     attention_vector = encoder_hidden_states @ attention_weights.T  # (n_features_enc, 1)
     return attention_vector
