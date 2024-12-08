71e13811c02cc049e8c5d2ecd77e1583a3b9f62c
k-gorbenko <164647543+k-gorbenko@users.noreply.github.com>
2024-12-08 23:16:08 +0300

    Update submission_template09.py

--- Deep Learning/submission_template09.py
+++ Deep Learning/submission_template09.py
@@ -14,7 +14,9 @@ def encoder_block(in_channels, out_channels, kernel_size, padding):
     # Параметры слоя conv заданы параметрами функции encoder_block. 
     # MaxPooling должен быть с ядром размера 2.
     block = nn.Sequential(
-        # ВАШ КОД ТУТ
+        nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding),
+        nn.ReLU(inplace=False),
+        nn.MaxPool2d(kernel_size=2)  # MaxPooling с ядром размера 2
     )
 
     return block
@@ -30,7 +32,9 @@ def decoder_block(in_channels, out_channels, kernel_size, padding):
     # Параметры слоя conv заданы параметрами функции encoder_block. 
     # Upsample должен быть со scale_factor=2. Тип upsampling (mode) можно выбрать любым.
     block = nn.Sequential(
-        # ВАШ КОД ТУТ
+        nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding),
+        nn.ReLU(inplace=False),
+        nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)  # Увеличение с масштабом 2
     )
 
     return block
@@ -48,35 +52,21 @@ class UNet(nn.Module):
         self.enc2_block = encoder_block(32, 64, 3, 1)
         self.enc3_block = encoder_block(64, 128, 3, 1)
 
-        # поймите, какие параметры должны быть у dec1_block, dec2_block и dec3_block
-        # dec1_block должен быть симметричен блоку enc3_block
-        # dec2_block должен быть симметричен блоку enc2_block
-        # но обратите внимание на skip connection между выходом enc2_block и входом dec2_block 
-        # (см что подается на вход dec2_block в функции forward)
-        # какое количество карт активации будет тогда принимать на вход dec2_block?
-        # также обратите внимание на skip connection между выходом enc1_block и входом dec3_block
-        # (см что подается на вход dec3_block в функции forward)
-        # какое количество карт активации будет тогда принимать на вход dec3_block?
-        self.dec1_block = decoder_block(?, ?, 3, 1)
-        self.dec2_block = decoder_block(?, ?, 3, 1)
-        self.dec3_block = decoder_block(?, out_channels, 3, 1)
+        # Определяем декодирующие блоки
+        self.dec1_block = decoder_block(128, 64, 3, 1)  # Симметричен enc3_block
+        self.dec2_block = decoder_block(64 + 64, 32, 3, 1)  # enc2_block + skip connection
+        self.dec3_block = decoder_block(32 + 32, out_channels, 3, 1)  # enc1_block + skip connection
 
     def forward(self, x):
-
         # downsampling part
         enc1 = self.enc1_block(x)
         enc2 = self.enc2_block(enc1)
         enc3 = self.enc3_block(enc2)
 
+        # upsampling part
         dec1 = self.dec1_block(enc3)
-        # из-за skip connection dec2 должен принимать на вход сконкатенированные карты активации
-        # из блока dec1 и из блока enc2. 
-        # конкатенация делается с помощью torch.cat
-        dec2 = self.dec2_block(torch.cat([dec1, enc2], 1))
-        # из-за skip connection dec3 должен принимать на вход сконкатенированные карты активации
-        # из блока dec2 и из блока enc1. 
-        # конкатенация делается с помощью torch.cat
-        dec3 = self.dec3_block(torch.cat([dec2, enc1], 1))
+        dec2 = self.dec2_block(torch.cat([dec1, enc2], 1))  # skip connection
+        dec3 = self.dec3_block(torch.cat([dec2, enc1], 1))  # skip connection
 
         return dec3
 
